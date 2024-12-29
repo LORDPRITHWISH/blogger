71ad9783834a4d0861c0c69e1b0cea681930ec1a
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2024-12-29 21:53:20 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+// const client = new Client();
+// client.setProject("676ff47100316161936f");
---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf = {
+    appwriteUrl: String(process.env.APPWRITE_ENDPOINT),
+    projectId: String(process.env.PROJ_ID),
+    databaseId: String(process.env.DATA_BASE_ID),
+    collectionId: String(process.env.COLLECTION_ID),
+    bucketId: String(process.env.BUCKET_ID),
+
+
+
+}
+
+export default conf;
