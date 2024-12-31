1d7ffef0dbb9f05470d5d9cf00026b2884f55a1c
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-01-01 02:55:59 +0530

    route

--- src/components/Authenticator.jsx
+++ src/components/Authenticator.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
 
 const Authenticator = ({children,authentication=true}) => {
-  return (
+
+  // if(authentication){
+
+    const logstate = useSelector(state => state.auth.status)
+    
+    const navigate = useNavigate()
+    
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(()=>{
+      if(authentication && !logstate){
+        navigate('/login')
+      }
+      else if(!authentication && logstate){
+        navigate('/')
+      }
+      setLoading(false)
+    },[logstate,authentication,navigate])
+    
+    
+  // }
+
+  return loading?null:(
     <div>
         {children}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,74 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import Authenticator from "./components/Authenticator.jsx";
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Post from "./pages/Post.jsx";
+import EditPosts from "./pages/EditPosts.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Authenticator authentication={false}>
+            <Login />
+          </Authenticator>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Authenticator authentication={false}>
+            <Signup />
+          </Authenticator>
+        ),
+      },
+      {
+        path: "/all_post",
+        element: (
+          <Authenticator authentication={true}>
+            <Post />
+          </Authenticator>
+        ),
+      },
+      {
+        path: "/edi/:slug",
+        element: (
+          <Authenticator authentication={true}>
+            <EditPosts />
+          </Authenticator>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: (
+          <Authenticator authentication={true}>
+            <Post />
+          </Authenticator>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
